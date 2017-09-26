dc7f1ebaa71d5d2f66b930f4de89ddcbd22d26d5
Sunheang Ty <tsun.heang@gmail.com>
2017-09-26 12:03:46 +1000

    5. RecordForm Unit Test Completed

--- test/unit/specs/RecordForm.spec.js
+++ test/unit/specs/RecordForm.spec.js
@@ -11,9 +11,49 @@ describe('RecordForm Unit Tests', () => {
     vm.$mount()
   })
 
+  it('should be correct components', () => {
+    expect(vm)
+      .to.be.a('object')
+      .that.is.an.instanceof(Constructor)
+  })
+
+  it('should has correct name', () => {
+    expect(vm.$options.name)
+      .to.be.a('string')
+      .that.equal('record-form')
+  })
+
+  it('should has correct data', () => {
+    expect(vm.$data)
+      .to.be.a('object')
+      .that.deep.equal({
+        title: null,
+        quantity: null,
+        date: null
+      })
+  })
+
+  it('should has correct methods', () => {
+    expect(vm.$options.methods)
+      .to.be.a('object')
+      .that.has.deep.property('addNewRecord')
+  })
+
   it('should render correct contents', () => {
-    expect(vm.$el.querySelector('legend').textContent)
-      .to.equal('Enter new record')
+    expect(vm.$el.querySelector('form')).to.exist
+    expect(vm.$el.querySelector('fieldset')).to.exist
+    expect(vm.$el.querySelector('legend').innerHTML).to.be.equal('Enter new record')
+    expect(vm.$el.querySelectorAll('label').item(0).innerHTML).to.be.equal('Title')
+    expect(vm.$el.querySelectorAll('label').item(1).innerHTML).to.be.equal('Quantity')
+    expect(vm.$el.querySelectorAll('label').item(2).innerHTML).to.be.equal('Date')
+    expect(vm.$el.querySelectorAll('input').item(0).getAttribute('type')).to.be.equal('text')
+    expect(vm.$el.querySelectorAll('input').item(1).getAttribute('type')).to.be.equal('text')
+    expect(vm.$el.querySelectorAll('input').item(2).getAttribute('type')).to.be.equal('text')
+    expect(vm.$el.querySelectorAll('input').item(0).getAttribute('placeholder')).to.be.equal('Title')
+    expect(vm.$el.querySelectorAll('input').item(1).getAttribute('placeholder')).to.be.equal('Quantity')
+    expect(vm.$el.querySelectorAll('input').item(2).getAttribute('placeholder')).to.be.equal('Date')
+    expect(vm.$el.querySelector('button').getAttribute('type')).to.be.equal('submit')
+    expect(vm.$el.querySelector('button').innerHTML).to.be.equal('Add Record')
   })
 
   after(() => {
