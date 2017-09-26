22e9474b514587ce32e9f1cfe943e7934e0af62e
Sunheang Ty <tsun.heang@gmail.com>
2017-09-26 14:52:02 +1000

    10. Prepare Project for CSV Reports

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app" class="uk-grid-small" uk-grid>
     <div>
       <record-form></record-form>
-      <report-form v-bind:reports="reports"></report-form>
     </div>
     <record-table>
       <record-row
@@ -19,16 +18,13 @@ import RecordForm from './components/RecordForm'
 import RecordTable from './components/RecordTable'
 import RecordRow from './components/RecordRow'
 
-import ReportForm from './components/ReportForm'
-
 export default {
   props: ['records', 'reports'],
   name: 'app',
   components: {
     RecordForm,
     RecordTable,
-    RecordRow,
-    ReportForm
+    RecordRow
   }
 }
 </script>
